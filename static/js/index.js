e1f5c38ea896517595f725cf10b2601b67667729
kongmengru <50727947+kongmengru@users.noreply.github.com>
2019-05-17 17:13:50 +0800

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -3,11 +3,11 @@
  */
 ;$(function () {
     //头部
-    $('header').load('header.html');
+   /* $('header').load('header.html');
     //底部
     $('footer').load('footer.html');
     
-   var testdata = 1;
+   var testdata = 1;*/
     
 });
 
