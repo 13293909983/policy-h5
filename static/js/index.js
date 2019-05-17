33bdf1261d12f0010ffada31c8b82b117c5dce32
kongmengru <50727947+kongmengru@users.noreply.github.com>
2019-05-17 19:35:24 +0800

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2017/3/7.
+ */
+;$(function () {
+    //头部
+   /* $('header').load('header.html');
+    //底部
+    $('footer').load('footer.html');
+    
+   var testdata = 1;*/
+    
+});
+
+
+
+
+
